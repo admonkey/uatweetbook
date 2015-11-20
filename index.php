318856fb39d5c0b435a2d07df6ebbcd26e7071cb
Jeff <jeff@jeffpuckett.com>
2015-11-20 00:19:41 -0600

    show help side nav

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 <div class='row'>
 
   <div class='col-md-6'>
+
+    <div class='well'>
+      <img alt='picture of a platypus' src='resources/img/Platypus-3.png' class='img-responsive'></img>
+      <p class='lead'>Please check out all of our <a href='Products/'>products</a>!</p>
+    </div><!-- /.well -->
+    
+  </div><!-- /.col-md-6 -->
+
+  <div class='col-md-6'>
     <div class='well'>
       <h3>Featured Product</h3>
       <div id='featured_product' class='well' style='background-color: white'>
@@ -25,7 +34,7 @@ $result = mysql_query($sql) or die(mysql_error());
 $row = mysql_fetch_assoc($result);
 
 echo "
-  <h4><a href='Products/Product.php?product_id=$product_id'>$row[product_name]</a></h4>
+  <h4><a href='Products/Product.php?product_id=$row[product_id]'>$row[product_name]</a></h4>
   <p class='lead'>$row[product_desc]</p>
 ";
 
@@ -34,11 +43,17 @@ echo "
     </div><!-- /.well -->
   </div><!-- /.col-md-6 -->
   
+</div><!-- /.row -->
+
+<div class='row'>
+
   <div class='col-md-6'>
 
     <div class='well'>
     
-    <img alt='picture of a platypus' src='resources/img/Platypus-3.png' class='img-responsive'></img>
+      <p>Lost? Just click the platypus icon <img src="<?php echo $relative_webpath;?>resources/img/favicon.ico"></img></a> anywhere to access the main side navigation menu.</p>
+    
+      <img alt='picture of a sidenav' src='resources/img/snapshot1.png' class='img-responsive'></img>
   
       <p>Learn more <a href='About/'>about <?=$site_title?></a></p>
 
@@ -47,19 +62,6 @@ echo "
     </div><!-- /.well -->
 
   </div><!-- /.col-md-6 -->
-
-</div><!-- /.row -->
-
-<div class='row'>
-
-  <div class='col-md-6'>
-
-    <div class='well'>
-      <p class='lead'>Please check out all of our <a href='Products/'>products</a>!</p>
-    </div><!-- /.well -->
-    
-  </div><!-- /.col-md-6 -->
-
 
 </div><!-- /.row -->
 
