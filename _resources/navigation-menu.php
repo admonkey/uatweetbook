c94943dd8e0d2e4f9b2a4e1e5a3a04a56c10622a
Ryan <ryan.gordy@gmail.com>
2015-12-01 22:20:33 -0600

    Added MYSQL Tab

--- _resources/navigation-menu.php
+++ _resources/navigation-menu.php
@@ -4,5 +4,5 @@
   <li><a href='$path_web_relative_root/Tables/'>Tables</a></li>
   <li><a href='$path_web_relative_root/SubTree/'>SubTree</a></li>
   <li><a href='$path_web_relative_root/LDAP/'>LDAP</a></li>
-
+  <li><a href='$path_web_relative_root/MYSQL_Login/'>MYSQL Login</a></li>
 ";?>
