7a6371306a79515d49d761e3d31fd02734b01667
Ryan <ryan.gordy@gmail.com>
2015-12-13 11:25:25 -0600

    stable form

---
+++ MYSQL_Login/index.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once '../includes/db_connect.php';
+include_once '../includes/functions.php';	
+include_once '../process_login.php';
+include_once '../logout.php';
+
+require_once('../_resources/header.php');
+$require_ssl = true;
+sec_session_start();
+
+
+if (login_check($mysqli) == true) {
+    $logged = 'in';
+} else {
+    $logged = 'out';
+}
+
+?>
+
+
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
+
+        <?php
+
+        if (isset($_GET['error'])) {
+            echo '<p class="error">Error Logging In!</p>';
+        }
+        ?> 
+
+        <form action="includes/process_login.php" method="post" name="login_form">                      
+            Email: <input type="text" name="email" />
+            Password: <input type="password" 
+                             name="password" 
+                             id="password"/>
+            <input type="button" 
+                   value="Login" 
+                   onclick="formhash(this.form, this.form.password);" /> 
+        </form>
+ 
+<?php
+        if (login_check($mysqli) == true) {
+                        echo '<p>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</p>';
+ 
+            echo '<p>Do you want to change user? <a href="includes/logout.php">Log out</a>.</p>';
+        } else {
+                        echo '<p>Currently logged ' . $logged . '.</p>';
+                        echo "<p>If you don't have a login, please <a href='register.php'>register</a></p>";
+                }
+
+			
+
+// footer
+require_once('../_resources/footer.php');
+
+?>      
+    
---
+++ includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "uatb");    // The database username. 
+define("PASSWORD", "J1bKm0MYgrZVQMxy");    // The database password. 
+define("DATABASE", "uatweetbook");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
